3dd7a00d558920f83786d4c45c3563924cf3e2e8
Priyanka Patel <ppatel@evokesolutions.com>
2019-05-13 01:17:03 -0400

    React Notice fix

--- src/EventListItem.js
+++ src/EventListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EventListItem = props => {
   return (
-    <a href={props.url} target="_blank">
+    <a href={props.url} target="_blank" rel="noopener noreferrer">
       {props.name}
     </a>
   );
